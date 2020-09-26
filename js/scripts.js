640bc0742e59dbc980acdc714663320beccb5123
Tin Zeljar <tinzeljar@gmail.com>
2020-09-27 00:36:24 +0200

    About done, Experience done

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,28 @@
 $(function () {
+    function scrollSpy() {
+        var scrollPos = $(window).scrollTop();
+        
+        if(scrollPos >= aboutPos && scrollPos < (experiencePos)){
+            $("li a").removeClass("text-muted");
+            $('#aboutItem').addClass("text-muted");
+
+        }
+        
+        else if (scrollPos >= experiencePos){
+            $("li a").removeClass("text-muted");
+            $('#experienceItem').addClass("text-muted");
+        }
+    }
+
     new WOW().init();
+
+    var bottomPage = $(window).scrollTop() + $(window).height() == $(document).height();
+    var aboutPos = $('#about').position().top;
+    var experiencePos = $('#experience').position().top;
+
+    scrollSpy();
+
+    $(window).scroll(function (event) {
+        scrollSpy();
+    });
 });
